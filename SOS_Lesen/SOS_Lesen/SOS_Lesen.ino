868214a48f94a9aefeefa75f87e99008c7b80a6b
peterschreiner42 <peterschreiner42@gmail.com>
2023-12-26 18:26:40 +0100

    Update SOS_Lesen.ino
    Der Code erkennt jetzt die Eingabe der Zeichen SOS.
    Der Code ist mit Erklärungen versehen. Diese werde ich von vervollständigen

--- SOS_Lesen/SOS_Lesen/SOS_Lesen.ino
+++ SOS_Lesen/SOS_Lesen/SOS_Lesen.ino
@@ -7,7 +7,7 @@
 // Either Key in a Morse code sequence using the button to decode,
 // or click in the field in the Serial Monitor, and
 // type letters followed by Enter to encode into Morse code.
-// The LED and tone will sound while you key in the sequence.
+// The LED while you key in the sequence.
 // This sketch decodes sequences with letters or numbers
 // and prints them to the serial monitor.
 // A beep sounds and "?" prints for unrecognized sequences.
@@ -41,13 +41,14 @@ int dotLength = 240;
 int dotSpace = dotLength;
 int dashLength = dotLength * 3;
 int letterSpace = dotLength * 3;
-int wordSpace = dotLength * 7;
 float wpm = 1200. / dotLength;
 
+int resetLength = 2000;
+
 int t1, t2, onTime, gap;
-bool newLetter, newWord, letterFound, keyboardText;
-int lineLength = 0;
-int maxLineLength = 20;
+bool newLetter, newWord, letterFound;
+// int lineLength = 0;
+// int maxLineLength = 20;
 
 char* letters[] = {
   ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",    // A-I
@@ -61,6 +62,7 @@ char* numbers[] = {
 };
 
 String dashSeq = "";
+String GesResult = "";
 char keyLetter, ch;
 int i, index;
 
@@ -70,77 +72,16 @@ void setup() {
   pinMode(tonePin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
   Serial.begin(9600);
-  Serial.println();
-  Serial.println("-------------------------------");
-  Serial.println("Morse Code decoder/encoder");
-  Serial.print("Speed=");
-  Serial.print(wpm);
-  Serial.print("wpm, ");
-  Serial.print("dot=");
-  Serial.print(dotLength);
-  Serial.println("ms");
 
-  // Test the LED and tone
-  tone(tonePin, toneFreq);
-  digitalWrite(ledPin, HIGH);
-  delay(2000);
-  digitalWrite(ledPin, LOW);
-  noTone(tonePin);
-  delay(600);
+  WriteStartScreen();
 
-  Serial.println();
-  Serial.println("-------------------------------");
-  Serial.println("Click field in Serial Monitor,");
-  Serial.println("type text and press Enter, or");
-  Serial.println("Key in Morse Code to decode:");
-  Serial.println("-------------------------------");
-
-  newLetter = false;  //if false, do NOT check for end of letter gap
-  newWord = false;    //if false, do NOT check for end of word gap
-  keyboardText = false;
+  //if false, do NOT check for end of letter gap
+  newLetter = false;
+  //if false, do NOT check for end of word gap
+  newWord = false;
 }
 
 void loop() {
-  // Check to see if something has been entered on the keyboard
-  if (Serial.available() > 0) {
-    // if (keyboardText == false) {
-    //   Serial.println();
-    //   Serial.println("-------------------------------");
-    // }
-
-    keyboardText = true;
-    // ch = Serial.read();
-    // if (ch >= 'a' && ch <= 'z') { ch = ch - 32; }
-
-    // if (ch >= 'A' && ch <= 'Z') {
-    //   Serial.print(ch);
-    //   Serial.print(" ");
-    //   Serial.println(letters[ch - 'A']);
-    //   flashSequence(letters[ch - 'A']);
-    //   delay(letterSpace);
-    // }
-
-    // if (ch >= '0' && ch <= '9') {
-    //   Serial.print(ch);
-    //   Serial.print(" ");
-    //   Serial.println(numbers[ch - '0']);
-    //   flashSequence(numbers[ch - '0']);
-    //   delay(letterSpace);
-    // }
-
-    if (ch == ' ') {
-      Serial.println("_");
-      delay(wordSpace);
-    }
-
-    // Print a header after last keyboard text
-    if (Serial.available() <= 0) {
-      Serial.println();
-      Serial.println("Enter text or Key in:");
-      Serial.println("-------------------------------");
-      keyboardText = false;
-    }
-  }
 
   if (digitalRead(buttonPin) == LOW)  //button is pressed
   {
@@ -148,7 +89,6 @@ void loop() {
     newWord = true;
     t1 = millis();               //time at button press
     digitalWrite(ledPin, HIGH);  //turn on LED and tone
-    tone(tonePin, toneFreq);
     delay(debounceDelay);
 
     while (digitalRead(buttonPin) == LOW)  // wait for button release
@@ -161,14 +101,15 @@ void loop() {
     t2 = millis();              //time at button release
     onTime = t2 - t1;           //length of dot or dash keyed in
     digitalWrite(ledPin, LOW);  //torn off LED and tone
-    noTone(tonePin);
 
     //check if dot or dash
     if (onTime <= dotLength * 1.5)  //allow for 50% longer
     {
       dashSeq = dashSeq + ".";
     }  //build dot/dash sequence
-    else {
+    else if (onTime >= resetLength) {
+      DoReset();
+    } else {
       dashSeq = dashSeq + "-";
     }
   }  //end button press section
@@ -176,6 +117,7 @@ void loop() {
   // look for a gap >= letterSpace to signal end letter
   // end of letter when gap >= letterSpace
   gap = millis() - t2;
+
   if (newLetter == true && gap >= letterSpace) {
     //check through letter sequences to find matching dash sequence
 
@@ -185,19 +127,9 @@ void loop() {
     for (i = 0; i <= 25; i++) {
       if (dashSeq == letters[i]) {
         keyLetter = i + 65;
+        GesResult += keyLetter;
         letterFound = true;
         break;  //don't keep checking if letter found
-      }
-    }
-
-    if (letterFound == false)  //now check for numbers
-    {
-      for (i = 0; i <= 10; i++) {
-        if (dashSeq == numbers[i]) {
-          keyLetter = i + 48;
-          letterFound = true;
-          break;  //don't keep checking if number found
-        }
       }
     }
 
@@ -205,42 +137,60 @@ void loop() {
 
     if (letterFound == false)  //buzz for unknown key sequence
     {
-      tone(tonePin, 100, 500);
       Serial.print("unbekannte key sequenz");
     }
 
     newLetter = false;  //reset
     dashSeq = "";
-    lineLength = lineLength + 1;
   }
 
-  // keyed letter has been identified and printed
-  // when gap is >= wordSpace, insert space between words
-  // lengthen the word space by 50% to allow for variation
+  // Der Schlüsselbuchstabe wurde identifiziert und gedruckt
+  // Wenn die Lücke >= Wortabstand ist, fügen Sie einen Zwischenraum zwischen den Wörtern ein
+  // Verlängern Sie den Wortabstand um 50%, um Variationen zu ermöglichen
 
-  if (newWord == true && gap >= wordSpace * 1.5) {
-    newWord = false;
-    if (keyLetter == "SOS") {
-      Serial.println("SOS Gefunden");
-    }
+  int index = GesResult.indexOf("SOS");
+  if (index != -1) {
+    Serial.println("");
+    Serial.println("SOS Gefunden");
+    GesResult = "";
+
+    // ************************************************************************
+    // ************************************************************************
+    // Hier die Aktion einbinden, die ausgeführt werden soll, wenn die SOS per Taster eingegeben wurde
+    // ************************************************************************
+    // ************************************************************************
   }
+}
 
-  // if (newWord == true && gap >= wordSpace * 1.5) {
-  //   newWord = false;
-  //   Serial.print("_");
-  //   lineLength = lineLength + 1;
-  //   // flash to indicate new word
+//*************************************************************
 
-  //   digitalWrite(ledPin, HIGH);
-  //   delay(25);
-  //   digitalWrite(ledPin, LOW);
-  // }
+void DoReset() {
+  newLetter = false;
+  newWord = false;
+  GesResult = "";
+  Serial.println("Reset");
+  delay(100);
+  WriteStartScreen();
+  return;
+}
 
-  // insert linebreaks
-  if (lineLength >= maxLineLength) {
-    Serial.println();
-    lineLength = 0;
-  }
+// Schreibt die Startmeldungen auf den Serial Monitor
+void WriteStartScreen() {
+  Serial.println();
+  Serial.println("-------------------------------");
+  Serial.println("SOS Recognition");
+  Serial.print("Speed=");
+  Serial.print(wpm);
+  Serial.print("wpm, ");
+  Serial.print("dot=");
+  Serial.print(dotLength);
+  Serial.println("ms");
+
+  Serial.println("For reset, hold the button for");
+  Serial.print(resetLength);
+  Serial.println(" milliseconds");
+  Serial.println("-------------------------------");
+  Serial.println("Ready");
 }
 
 void flashSequence(char* sequence) {
